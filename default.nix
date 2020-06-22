b9d3d690e9ff6e94946423fbf5d46a39f87245e4
Jakub Kopański <jakub@famisoft.pl>
2020-06-22 18:49:53 +0200

    Build all components

--- default.nix
+++ default.nix
@@ -8,7 +8,10 @@ let
   pkgSet = haskell.mkCabalProjectPkgSet {
     plan-pkgs = import ./pkgs.nix;
     pkg-def-extras = [];
-    modules = [];
+    modules = [
+      { packages.lifted-async.patches = [ ./patches/lifted-async-0.10.0.6.patch ]; }
+      { packages."QuickCheck".patches = [ ./patches/QuickCheck-2.14.patch ]; }
+    ];
   };
 
 in
